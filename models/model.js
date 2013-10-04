8e38388860f7aba8707b40825e703351f4ad5a2b
Etienne <etienne.dodat@gmail.com>
2013-10-05 01:25:37 +0200

    updated: db connection configurable

--- models/model.js
+++ models/model.js
@@ -9,83 +9,81 @@
 // INITIALIZATION //
 ////////////////////
 
-var mongo = require('mongojs');
+var mongo = require('mongojs'),
+    config = require('../config/mongo.js');
 
 /////////////
 // PRIVATE //
 /////////////
 
 // Connect to database
-var db = mongo.connect('localhost/admin');
+var db = mongo.connect(config.connectionString);
 
 ////////////
 // PUBLIC //
 ////////////
 
-// Static class or singleton (equivalent in Javascript)
-var Model = {};
-
 /**
- * Initialization function for Model's subclasses.
- * Augments subclass capabilities by adding static functions.
+ * Initialization function for Model's submodules.
+ * Augments submodule capabilities by adding static functions.
  *
- * Usage (subclass definition) :
+ * Usage (submodule definition) :
  *      var MyModel = {};
  *      Model.declare(MyModel, 'mycollection');
  *      ... define custom methods...
  *
- * @param Subclass
- * @param collectionName : Mongo collection where subclass objects are stored
+ * @param Submodule "exports"
+ * @param collectionName : Mongo collection where submodule objects are stored
  */
-Model.declare = function (Subclass, collectionName){
+module.exports.declare = function (submodule, collectionName){
     // create _collection variable in subclass
-    var collection = Model._db.collection(collectionName);
-    Subclass._collection  = collection;
-    // augment subclass with MongoJS functions
-    Subclass.findById = Model.findById;
-    Subclass.find = Model.find;
-    Subclass.findOne = Model.findOne;
-    Subclass.findAndModify = Model.findAndModify;
-    Subclass.update = Model.update;
-    Subclass.save = Model.save;
-    Subclass.remove = Model.remove;
+    var collection = module.exports._db.collection(collectionName);
+    submodule._collection  = collection;
+    // augment submodule with MongoJS functions
+    submodule.findById = module.exports.findById;
+    submodule.find = module.exports.find;
+    submodule.findOne = module.exports.findOne;
+    submodule.findAndModify = module.exports.findAndModify;
+    submodule.update = module.exports.update;
+    submodule.save = module.exports.save;
+    submodule.remove = module.exports.remove;
 };
 
 /**
  * DB connection handler.
  */
-Model._db = db;
+module.exports._db = db;
 
 /**
  * Static functions mapped to MongoJS collection functions.
  * These functions are made available to Model's subclasses by "Model.declare" function above.
  */
 
-Model.find = function (){
+module.exports.find = function (){
     return this._collection.find.apply(this._collection, arguments);
 };
 
-Model.findOne = function () {
+module.exports.findOne = function () {
     return this._collection.findOne.apply(this._collection, arguments);
 };
 
-Model.findById = function (id, callback) {
+module.exports.findById = function (id, callback) {
     return this._collection.findOne({ _id: Model.ObjectId(id) }, callback);
 };
 
-Model.findAndModify = function () {
+module.exports.findAndModify = function () {
     return this._collection.findAndModify.apply(this._collection, arguments);
 };
 
-Model.update = function () {
+module.exports.update = function () {
     return this._collection.update.apply(this._collection, arguments);
 };
 
-Model.save = function () {
+module.exports.save = function () {
     return this._collection.save.apply(this._collection, arguments);
 };
 
-Model.remove = function () {
+module.exports.remove = function () {
     return this._collection.remove.apply(this._collection, arguments);
 };
 
@@ -95,10 +93,7 @@ Model.remove = function () {
  * @param id (string or ObjectId)
  * @returns ObjectId
  */
-Model.ObjectId = function (id){
+module.exports.ObjectId = function (id){
     if (typeof id == 'string') return Model._db.ObjectId(id);
     else return id;
 };
-
-
-module.exports = Model;
